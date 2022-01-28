7ac6cf587a1a72200adcef74aae9405a27b8d148
gustonecrush <codeboxezz@gmail.com>
2022-01-29 00:13:42 +0700

    add directory

---
+++ 20220129 - PBO 201/src/id/college/Main.java
@@ -0,0 +1,19 @@
+package id.college;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner inputUser = new Scanner(System.in);
+        int score;
+
+        System.out.print("input your score : ");
+        score = inputUser.nextInt();
+
+        if( score < 55 ) {
+            System.out.println("Student is fail.");
+        }
+
+    }
+}
